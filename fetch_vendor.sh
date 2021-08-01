bd5c72a5f55e62f56e7552db30172e667a102327
Kyle Laker <kyle@laker.email>
2021-08-01 15:00:55 -0400

    Clean vendored code before cloning

--- fetch_vendor.sh
+++ fetch_vendor.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p runtime/vendor
+rm -rf runtime/vendor/updatechecker
 temp_dir="$(mktemp -d)"
 git clone https://github.com/kylelaker/updatechecker "$temp_dir"
 cp -r "$temp_dir/updatechecker" "runtime/vendor"
