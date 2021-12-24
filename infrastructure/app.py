7b358eea51386f0424e891192dee64b156674465
Kyle Laker <kyle@laker.email>
2021-12-23 20:03:38 -0500

    Support custom domains

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -6,9 +6,10 @@ from stacks.chaliceapp import ChaliceApp
 
 app = cdk.App()
 env = cdk.Environment(
-    account=os.environ["CDK_DEFAULT_ACCOUNT"],
-    region=os.environ["CDK_DEFAULT_REGION"]
+    account=os.environ["CDK_DEFAULT_ACCOUNT"], region=os.environ["CDK_DEFAULT_REGION"]
 )
-ChaliceApp(app, "updatecheckerv2", env=env)
+ChaliceApp(
+    app, "updatecheckerv2", env=env, domain_name=app.node.try_get_context("DomainName")
+)
 
 app.synth()
--- infrastructure/stacks/chaliceapp.py
+++ infrastructure/stacks/chaliceapp.py
@@ -1,7 +1,14 @@
 import os
 
-from aws_cdk import aws_dynamodb as dynamodb, aws_sns as sns, core as cdk
-import chalice
+from aws_cdk import (
+    aws_certificatemanager as acm,
+    aws_apigateway as apigw,
+    aws_dynamodb as dynamodb,
+    aws_route53 as dns,
+    aws_sns as sns,
+    core as cdk,
+)
+import jsii
 from chalice.cdk import Chalice
 
 
@@ -10,28 +17,72 @@ RUNTIME_SOURCE_DIR = os.path.join(
 )
 
 
+@jsii.implements(dns.IAliasRecordTarget)
+class ChaliceCustomDomain:
+    def __init__(self, apigw_domain: apigw.CfnDomainName):
+        self.dns_name = apigw_domain.attr_distribution_domain_name
+        self.hosted_zone_id = apigw_domain.attr_distribution_hosted_zone_id
+
+    def bind(self, record, zone):
+        return dns.AliasRecordTargetConfig(
+            dns_name=self.dns_name, hosted_zone_id=self.hosted_zone_id
+        )
+
+
 class ChaliceApp(cdk.Stack):
-    def __init__(self, scope, id, **kwargs):
+    def __init__(self, scope, id, domain_name=None, **kwargs):
         super().__init__(scope, id, **kwargs)
         self.dynamodb_table = self._create_ddb_table()
         self.sns_topic = self._create_sns_topic()
+        stage_config = {
+            "environment_variables": {
+                "APP_TABLE_NAME": self.dynamodb_table.table_name,
+                "APP_TABLE_STREAM": self.dynamodb_table.table_stream_arn,
+                "NOTIFY_TOPIC": self.sns_topic.topic_arn,
+            },
+            "automatic_layer": True,
+            "lambda_memory_size": 384,
+        }
+        if domain_name:
+            self.api_fqdn = f"updatechecker.{domain_name}"
+            zone = self._get_hosted_zone(domain_name)
+            self.certificate = self._create_certificate(zone)
+            stage_config["api_gateway_custom_domain"] = {
+                "domain_name": self.api_fqdn,
+                "certificate_arn": self.certificate.certificate_arn,
+            }
         self.chalice = Chalice(
             self,
             "ChaliceApp",
             source_dir=RUNTIME_SOURCE_DIR,
-            stage_config={
-                "environment_variables": {
-                    "APP_TABLE_NAME": self.dynamodb_table.table_name,
-                    "APP_TABLE_STREAM": self.dynamodb_table.table_stream_arn,
-                    "NOTIFY_TOPIC": self.sns_topic.topic_arn,
-                },
-                "automatic_layer": True,
-                "lambda_memory_size": 384,
-            },
+            stage_config=stage_config,
         )
         self.dynamodb_table.grant_read_write_data(self.chalice.get_role("DefaultRole"))
         self.dynamodb_table.grant_stream_read(self.chalice.get_role("DefaultRole"))
         self.sns_topic.grant_publish(self.chalice.get_role("DefaultRole"))
+
+        if domain_name:
+            self._create_dns_records(zone)
+
+    def _get_hosted_zone(self, domain_name):
+        return dns.HostedZone.from_lookup(self, "DomainDns", domain_name=domain_name)
+
+    def _create_certificate(self, hosted_zone):
+        return acm.DnsValidatedCertificate(
+            self, "Certificate", hosted_zone=hosted_zone, domain_name=self.api_fqdn
+        )
+
+    def _create_dns_records(self, zone):
+        apigw_domain: apigw.CfnDomainName = self.chalice.get_resource(
+            "ApiGatewayCustomDomain"
+        )
+        target = dns.RecordTarget.from_alias(ChaliceCustomDomain(apigw_domain))
+        dns.ARecord(
+            self, "ApiGwAlias", zone=zone, target=target, record_name=self.api_fqdn
+        )
+        dns.AaaaRecord(
+            self, "ApiGwAaaalias", zone=zone, target=target, record_name=self.api_fqdn
+        )
 
     def _create_sns_topic(self):
         sns_topic = sns.Topic(
